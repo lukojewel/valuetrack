864f6d5c7202573325e4c8dfffd66b0dbf5c5ce1
Jewel <jikkurox@gmail.com>
2019-07-31 22:21:14 +0530

    add customer

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-// const server = "http:127.0.0.1:5000/";
-const server = "https://bdtgght4ra.execute-api.ap-south-1.amazonaws.com/prod/";
+const server = "http:127.0.0.1:5000/";
+// const server = "https://bdtgght4ra.execute-api.ap-south-1.amazonaws.com/prod/";
 
 function login(loginType) {
 	console.log(loginType);
-	if (loginType) {
+	if (loginType == 1) {
+		console.log("inside if");
 		var username = $("#brancheid").val();
 		var password = $("#branchpass").val();
 		if (username && password) {
@@ -30,6 +31,10 @@ function login(loginType) {
 								"loggedIn",
 								JSON.stringify(response.access_token)
 							);
+							sessionStorage.setItem(
+								"userType",
+								JSON.stringify("Branch Office")
+							);
 							window.location.href = "home.html";
 						} else {
 							$("#errText1").text(response.message);
@@ -44,14 +49,15 @@ function login(loginType) {
 			$("#errText1").text("All fields are mandatory");
 		}
 	} else {
-		var username = $("#haedeid").val();
-		var password = $("#haedpass").val();
+		console.log("inside else");
+		var username = $("#headeid").val();
+		var password = $("#headpass").val();
 		if (username && password) {
 			var Auth = window.btoa(username.concat(":").concat(password));
 			console.log(Auth);
 			$.ajax({
 				type: "GET",
-				url: server + "headoffice/auth/login",
+				url: server + "head/auth/login",
 				// dataType: "json",
 				headers: {
 					Accept: "application/json",
@@ -66,6 +72,14 @@ function login(loginType) {
 					function(response) {
 						console.log(response);
 						if (response.status) {
+							sessionStorage.setItem(
+								"loggedIn",
+								JSON.stringify(response.access_token)
+							);
+							sessionStorage.setItem(
+								"userType",
+								JSON.stringify("Head Office")
+							);
 							window.location.href = "home.html";
 						} else {
 							$("#errText2").text(response.message);
@@ -85,4 +99,49 @@ function login(loginType) {
 function logout() {
 	sessionStorage.removeItem("loggedIn");
 	window.location = "index.html";
+}
+
+function addCustomer() {
+	var name = $("#Name").val();
+	var email = $("#Email").val();
+	var dob = $("#DOB").val();
+	var branch = $("#Branch").val();
+	var contact = $("#Contact").val();
+	var due = $("#Dues").val();
+
+	if (name && email && dob && branch && contact && due) {
+		$.ajax({
+			type: "POST",
+			url: server + "branch/customer/add",
+			// dataType: "json",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json"
+			},
+			data: JSON.stringify({
+				name: name,
+				email: email,
+				dob: dob,
+				branch: branch,
+				contact: contact,
+				due: due
+			})
+		})
+			.done(
+				function(response) {
+					console.log(response);
+					if (response.status) {
+						window.location.href = "view-customers.html";
+					} else {
+						$("#customerError").text(response.message);
+					}
+				}.bind(this)
+			)
+			.fail(err => {
+				console.log(err);
+				$("#customerError").text("Network error occurred.");
+			});
+	} else {
+		$("#customerError").text("All fields are mandatory");
+	}
 }
