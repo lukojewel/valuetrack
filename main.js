4001c22c026d799cca192ea9b4f5e151ce60fd9d
Jewel <jikkurox@gmail.com>
2019-08-01 14:10:42 +0530

    changed url

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const server = "http:127.0.0.1:5000/";
-// const server = "https://bdtgght4ra.execute-api.ap-south-1.amazonaws.com/prod/";
+// const server = "http:127.0.0.1:5000/";
+const server = "https://bdtgght4ra.execute-api.ap-south-1.amazonaws.com/prod/";
 
 function login(loginType) {
 	console.log(loginType);
